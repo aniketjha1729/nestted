fdebaa56b2c579a51ef0e4a97222eb830d1583ae
aniketjha1729 <kumaraniket1729@gmail.com>
2023-04-19 02:29:46 +0530

    bootstrap configured and some added

--- src/Abc.js
+++ src/Abc.js
@@ -1,25 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const RecursiveComponent = ({ typeOfField }) => {
-  const [type, setType] = useState("string");
-
+  const [type, setType] = useState("");
+  const [showButton, setShowAddButton] = useState(false);
   if (typeOfField === "string" || typeOfField === "boolean") {
     return null; // stop case
   }
 
+  console.log(type);
+
   const bgImageStyles = {
     paddingLeft: `40px`,
+    backgroundColor: "red",
   };
 
   return (
     <div style={{ ...bgImageStyles }}>
-      <p>Depth: type</p>
       <select onChange={(e) => setType(e.target.value)}>
         <option value="string">string</option>
         <option value="boolean">boolean</option>
         <option value="object">object</option>
       </select>
-      <RecursiveComponent typeOfField={type} />
+      {showButton === false && (
+        <button onClick={() => setShowAddButton(true)}>+</button>
+      )}
+      {type === "object" && showButton === true && (
+        <div className="">
+          <span></span>
+          <span>
+            <RecursiveComponent typeOfField={type} />
+          </span>
+        </div>
+      )}
     </div>
   );
 };
